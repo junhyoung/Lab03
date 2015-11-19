f37b69ba85ce02b6233c9b899cabe4f591bd682e
junhyeong <zz7193@naver.com>
2015-11-19 13:25:08 +0900

    Message intent

--- app/src/main/java/github/com/junhyoung/lab03/MainActivity.java
+++ app/src/main/java/github/com/junhyoung/lab03/MainActivity.java
@@ -1,5 +1,6 @@
 package github.com.junhyoung.lab03;
 
+import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.provider.MediaStore;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btStop;
     MediaPlayer mMplayer; // Prepare mp3 file for MediaPlayer
     // Prepare mp3 file for MediaPlayer
+    Button btEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
         mMplayer = new MediaPlayer();
         btStart = (Button) findViewById(R.id.bt_start);
         btStop = (Button) findViewById(R.id.bt_stop);
+        btEdit=(Button)findViewById(R.id.bt_edit);
+
         try {
-            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.boysandgirls);
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.boysandgirls);//raw내의 파일은 무조선 소문자
             mMplayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -51,6 +56,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        btEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getApplicationContext(), NewActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("github.com.junhyoung.lab03");
+        TextView textView = (TextView)findViewById(R.id.text);
+        textView.setText(message);
 
     }
 
--- app/src/main/java/github/com/junhyoung/lab03/NewActivity.java
+++ app/src/main/java/github/com/junhyoung/lab03/NewActivity.java
@@ -1,13 +1,41 @@
 package github.com.junhyoung.lab03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewActivity extends AppCompatActivity {
 
+    Button btOk;
+    Button btCc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+
+        btOk=(Button)findViewById(R.id.bt_ok);
+        btCc=(Button)findViewById(R.id.bt_cc);
+        btOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText)findViewById(R.id.editText);
+                String message = editText.getText().toString();
+                Intent intent;
+                intent=new Intent(NewActivity.this,MainActivity.class);
+                intent.putExtra("github.com.junhyoung.lab03", message);
+                startActivity(intent);
+            }
+        });
+        btCc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
+
 }
